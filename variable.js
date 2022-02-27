de8b78d38cf20cd86a30bcab23f309fe526251c6
chatnatee <chatnatee_khamlue@hotmail.com>
2022-02-27 17:18:44 -0500

    first

---
+++ variable.js
@@ -0,0 +1,46 @@
+// comments can make code readable
+//  Welcome to 30DaysOfJavaScript
+/* comments can make code readable, 
+easy to reuse and informative */
+
+var string = 'tomato';
+var tomato = true;
+var peanut;
+var butter = null ;
+
+console.log(typeof 'tomato');
+console.log(typeof true);
+console.log(typeof peanut);
+console.log(typeof butter);
+
+var pizza;
+var coconut;
+var cola;
+var lamon;
+
+var hat = 1;
+var mat = 2;
+var rat = 3;
+var chad = 4;
+
+var first_name = "George";
+var Last_name = "Henri";
+var Marial_S = "Single";
+var age = 69;
+
+var ïnfo = "George",
+Last_name = "Henri",
+Marial_S = "Single",
+age = 69;
+
+console.log(info,Last_name,age)
+
+var myAge = 420;
+var yourAge = 56;
+
+console.log(myAge);
+console.log(yourAge);
+
+
+
+
